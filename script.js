0050de488fab53addbde52c6b1bd5bfec022ebc7
maestradorafa4-ctrl <maestradorafa4@gmail.com>
2026-09-24 00:43:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 ========================================================= */
 
 const API_URL =
-  'https://script.google.com/macros/s/AKfycbx-KpDFXlX152DoB0Rk4GfFkmv1KSEhWiaFco5BX0GskAzVhgy-UYiDBLURDtnWN6/exec';
+  'https://script.google.com/macros/s/AKfycbzLYo3JoGK835gj_BVmqEEKy3FIT3n9KOWVc-j2sS_50S6fxNS2flKNtvtl-a0gYbwk/exec';
 
 
 /* =========================================================
